08ebcfb150e2047818f4e7cb2b263c36c10edb1e
mtp22peng <mt.peng@gmail.com>
2016-10-14 10:05:51 +0800

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,12 @@
-
 # section 0
 library(dplyr)
-library(tidyr)
-
 c0 <- read.table("./UCI HAR Dataset/activity_labels.txt",stringsAsFactors=FALSE )
 c1 <- read.table("./UCI HAR Dataset/features.txt",stringsAsFactors=FALSE )
 
-
 # section 1 
-d1 <- read.table("./UCI HAR Dataset/train/X_train.txt")
+d1 <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = c1[,2])
 d22 <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-d22 <- mutate(d22, subject2 = paste("subject", d22[,1]))
-names(d22)[1]<-"subject1"
-names(d22)[2]<-"subject"
+names(d22)[1]<-"subject"
 d2 <- select(d22, subject)
 d3 <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 names(d3)[1]<-"activity"
@@ -20,9 +14,8 @@ d4 <- c0[d3[,1],2]
 
 
 # section 2
-c2 <- select(d1,   V1: V6)
-names(c2)[1:6]<-c1[1:6,2]
-fin1 <- cbind(d2,d4,c2)
+c2 <- select(d1, contains("mean.."), contains("std.."))
+fin1 <- tbl_df(cbind(d2,d4,c2))
 names(fin1)[2] <- "activity"
 rm("d1")
 rm("d2")
@@ -32,11 +25,9 @@ rm("d4")
 
 
 # section 3
-e1 <- read.table("./UCI HAR Dataset/test/X_test.txt")
+e1 <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = c1[,2])
 e22 <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-e22 <- mutate(e22, subject2 = paste("subject", e22[,1]))
-names(e22)[1]<-"subject1"
-names(e22)[2]<-"subject"
+names(e22)[1]<-"subject"
 e2 <- select(e22, subject)
 e3 <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 names(e3)[1]<-"activity"
@@ -44,9 +35,8 @@ e4 <- c0[e3[,1],2]
 
 
 # section 4
-f2 <- select(e1,   V1: V6)
-names(f2)[1:6]<-c1[1:6,2]
-fin2 <- cbind(e2,e4,f2)
+f2 <- select(e1, contains("mean.."), contains("std.."))
+fin2 <- tbl_df(cbind(e2,e4,f2))
 names(fin2)[2] <- "activity"
 rm("e1")
 rm("e2")
@@ -56,83 +46,20 @@ rm("e4")
 
 
 # section 5
-final <- full_join(fin1, fin2, by =  
-                           c("subject" , "activity" )  )
-final1 <- gather(final, key = "variable" , value = "value", -c(1:2)
-)
+final <- rbind(fin1, fin2)
+final1 <- arrange(final, subject) %>% group_by( subject, activity)
+final1 <-  final1 %>% setNames(gsub("^f", "Frequency_", names(.)))
+final1 <-   final1 %>%  setNames(gsub("^t", "Time_", names(.)))
+final1 <-  final1 %>%   setNames(gsub("Acc", "Accelerometer_", names(.)))
+final1 <-  final1 %>%   setNames(gsub("Gyro", "Gyroscope_", names(.)))
+final1 <-  final1 %>%   setNames(gsub("Mag", "Magnitude_", names(.)))
+final1 <- final1 %>% setNames(gsub("mean\\.\\.", "Mean", names(.)))
+final1 <- final1 %>% setNames(gsub("std\\.\\.", "Std", names(.)))
+final1 <-   final1 %>%  setNames(gsub("\\.", "", names(.)))
+
+
 
 # section 6
-# subject
-final2 <- filter(final1, variable == c("tBodyAcc-mean()-X.x" , 
-                                       "tBodyAcc-mean()-X.y" ) )
-final2 <- group_by(final2, subject )
-out2 <- summarise(final2,  "tBodyAcc-mean()-X" = mean(value, na.rm = TRUE))
-
-final3 <- filter(final1, variable == c("tBodyAcc-mean()-Y.x" , 
-                                       "tBodyAcc-mean()-Y.y" ) )
-final3 <- group_by(final3, subject )
-out3 <- summarise(final3,  "tBodyAcc-mean()-Y" = mean(value, na.rm = TRUE))
-final4 <- filter(final1, variable == c("tBodyAcc-mean()-Z.x" , 
-                                       "tBodyAcc-mean()-Z.y" ) )
-final4 <- group_by(final4, subject )
-out4 <- summarise(final4,  "tBodyAcc-mean()-Z" = mean(value, na.rm = TRUE))
-final5 <- filter(final1, variable == c("tBodyAcc-std()-X.x" , 
-                                       "tBodyAcc-std()-X.y" ) )
-final5 <- group_by(final5, subject )
-out5 <- summarise(final5,  "tBodyAcc-std()-X" = mean(value, na.rm = TRUE))
-final6 <- filter(final1, variable == c("tBodyAcc-std()-Y.x" , 
-                                       "tBodyAcc-std()-Y.y" ) )
-final6 <- group_by(final6, subject )
-out6 <- summarise(final6,  "tBodyAcc-std()-Y" = mean(value, na.rm = TRUE))
-final7 <- filter(final1, variable == c("tBodyAcc-std()-Z.x" , 
-                                       "tBodyAcc-std()-Z.y" ) )
-final7 <- group_by(final7, subject )
-out7 <- summarise(final7,  "tBodyAcc-std()-Z" = mean(value, na.rm = TRUE))
-
-out8 <- full_join(out2, out3, by = "subject") %>% 
-        full_join(out4, by = "subject") %>% 
-        full_join(out5, by = "subject") %>% 
-        full_join(out6, by = "subject") %>% 
-        full_join(out7, by = "subject") 
-out9 <- gather(out8, key = "variable" , value = "value", -c(1))
-names(out9)[1] <- "study"
-
-# section 7
-# activity
-final22 <- filter(final1, variable == c("tBodyAcc-mean()-X.x" , 
-                                        "tBodyAcc-mean()-X.y" ) )
-final22 <- group_by(final22, activity )
-out22 <- summarise(final22,  "tBodyAcc-mean()-X" = mean(value, na.rm = TRUE))
-final33 <- filter(final1, variable == c("tBodyAcc-mean()-Y.x" , 
-                                        "tBodyAcc-mean()-Y.y" ) )
-final33 <- group_by(final33, activity )
-out33 <- summarise(final33,  "tBodyAcc-mean()-Y" = mean(value, na.rm = TRUE))
-final44 <- filter(final1, variable == c("tBodyAcc-mean()-Z.x" , 
-                                        "tBodyAcc-mean()-Z.y" ) )
-final44 <- group_by(final44, activity )
-out44 <- summarise(final44,  "tBodyAcc-mean()-Z" = mean(value, na.rm = TRUE))
-final55 <- filter(final1, variable == c("tBodyAcc-std()-X.x" , 
-                                        "tBodyAcc-std()-X.y" ) )
-final55 <- group_by(final55, activity )
-out55 <- summarise(final55,  "tBodyAcc-std()-X" = mean(value, na.rm = TRUE))
-final66 <- filter(final1, variable == c("tBodyAcc-std()-Y.x" , 
-                                        "tBodyAcc-std()-Y.y" ) )
-final66 <- group_by(final66, activity )
-out66 <- summarise(final66,  "tBodyAcc-std()-Y" = mean(value, na.rm = TRUE))
-final77 <- filter(final1, variable == c("tBodyAcc-std()-Z.x" , 
-                                        "tBodyAcc-std()-Z.y" ) )
-final77 <- group_by(final77, activity )
-out77 <- summarise(final77,  "tBodyAcc-std()-Z" = mean(value, na.rm = TRUE))
-out88 <- full_join(out22, out33, by = "activity") %>% 
-        full_join(out44, by = "activity") %>% 
-        full_join(out55, by = "activity") %>% 
-        full_join(out66, by = "activity") %>% 
-        full_join(out77, by = "activity") 
-out99 <- gather(out88, key = "variable" , value = "value", -c(1))
-names(out99)[1] <- "study"
-
-# section 8
-out <- full_join(out9, out99, , rm.na = TRUE)
-
+out <- summarise_each (final1, funs(mean))
 write.table(out, file = "out.txt", sep = ",", 
             col.names = NA, qmethod = "double")
